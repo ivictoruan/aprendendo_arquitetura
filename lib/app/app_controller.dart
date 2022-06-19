8b0e7e06e647d0bdf0685c4c0e9ecc4b1e3ccbf3
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-06-19 11:13:22 -0300

    FEAT[ARQUITETURA#3 - Service]: Criação de uma camada de services, no caso, local storage. Criou-se um contrato(abs class) assim como uma implementação desta com SharedPreferences.

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,14 +1,19 @@
 // import 'package:flutter/cupertino.dart';
 
+import 'package:aprendendo_arquitetura/app/interfaces/local_storage_interface.dart';
 import 'package:aprendendo_arquitetura/app/models/appconfig_model.dart';
+import 'package:aprendendo_arquitetura/app/services/shared_local_storage_service.dart';
 import 'package:flutter/cupertino.dart';
 
 class AppController{
 
   static final AppController instance = AppController._(); // Singleton para instanciar o controller na aplicação
-  AppController._(); // construtor privado: pode acessar a instancia apenas dentro da classe específica!
+  AppController._() {
+    storage.get('isDark').then((value) => {
+      if(value != null) config.themeSwitch.value = value 
+    });
+  }// construtor privado: pode acessar a instancia apenas dentro da classe específica!
 
-  // final themeSwitch = ValueNotifier<bool>(false);
 
   AppconfigModel config = AppconfigModel();
 
@@ -16,7 +21,10 @@ class AppController{
 
   ValueNotifier<bool> get themeSwitch => config.themeSwitch;
 
+  final LocalStorageInterface storage = SharedLocalStorageService(); // tipa a interface e instancia a implementação
+
   changeTheme(bool value){
     config.themeSwitch.value = value;
+    storage.put('isDark', value); // salva o valor<bool> em relação a String (localmente)
   }
 }
